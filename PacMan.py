25ebf7323732f40cf257c1b4f9f20c9616e931a9
arnavkrishnan <arnav.s.krishnan@gmail.com>
2021-06-17 09:03:39 -0700

    fix collosions

--- PacMan.py
+++ PacMan.py
@@ -3,12 +3,9 @@ import time
 
 rows = 11
 columns = 20
-px = (0)
-py = (0)
 grid_size = 50
-pa = 90
-grid_x = ()
-grid_y = ()
+grid_x = 0
+grid_y = 0
 screen = pygame.display.set_mode([columns*grid_size,rows*grid_size])
 screen.fill((0,0,0))
 pygame.display.set_caption('my first game')
@@ -31,14 +28,10 @@ map = [ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 def draw_player(pacman):
-    #rect = pacman.get_rect()
-    #px = 3*grid_size+grid_size//2
-    #py = 3*grid_size+grid_size//2
-    #pa = 0
-    #rect.center = (px, py)
-    #screen.blit(pacman, (0, 0))
+    rect = pacman.get_rect()
+    rect.center = (px, py)
     pacman = pygame.transform.rotate(pacman, pa)
-    screen.blit(pacman, (px, py))
+    screen.blit(pacman, rect)
 
 def eat_pellets():
     gx = px//grid_size
@@ -47,13 +40,13 @@ def eat_pellets():
         map[gy][gx] = 3
 
 walls = [[0, 0, 18, 0], [0, 0, 0, 10], [0, 10, 18, 10], [18,0,18,10],
-		 [2, 2, 3, 2], [2, 2, 2, 4], [5, 0, 5, 2], [4, 4, 5, 4],
-		 [2, 8, 3, 8], [2, 6, 2, 8], [5, 8, 5, 10], [4, 6, 5, 6],
-		 [15, 2, 16, 2], [16, 2, 16, 4], [13, 0, 13, 2], [13, 4, 14, 4],
-		 [15, 8, 16, 8], [16, 6, 16, 8], [13, 8, 13, 10], [13, 6, 14, 6],
-		 [7, 2, 11,2], [7, 8, 11, 8], [7, 6, 11, 6],
-		 [7, 4, 8, 4], [10, 4, 11, 4], [7, 4, 7, 6], [11, 4, 11, 6],
-		 [9, 4, 9, 4]] # last one is the door
+         [2, 2, 3, 2], [2, 2, 2, 4], [5, 0, 5, 2], [4, 4, 5, 4],
+         [2, 8, 3, 8], [2, 6, 2, 8], [5, 8, 5, 10], [4, 6, 5, 6],
+         [15, 2, 16, 2], [16, 2, 16, 4], [13, 0, 13, 2], [13, 4, 14, 4],
+         [15, 8, 16, 8], [16, 6, 16, 8], [13, 8, 13, 10], [13, 6, 14, 6],
+         [7, 2, 11,2], [7, 8, 11, 8], [7, 6, 11, 6],
+         [7, 4, 8, 4], [10, 4, 11, 4], [7, 4, 7, 6], [11, 4, 11, 6],
+         [9, 4, 9, 4]] # last one is the door
 
 
 def draw_map():
@@ -64,16 +57,15 @@ def draw_map():
             grid_x = j*grid_size
             if map[i][j] == 1:
                 pygame.draw.circle(screen, "red", (grid_x + grid_size//2, grid_y + grid_size//2), 4)
-            elif map[i][j] == 0:
-                pygame.draw.rect(screen, "blue", (grid_x, grid_y, grid_size, grid_size))
+            #elif map[i][j] == 0:
+             #   pygame.draw.rect(screen, "blue", (grid_x, grid_y, grid_size, grid_size))
     draw_walls()
 
-walls = []
 wall_rects = []
 def convert_walls():
     for i in range(len(walls)):
-        gx1, gx2, gy1, gy2 = walls[i]
-        if gx1 == gx2:
+        gx1, gy1, gx2, gy2 = walls[i]
+        if gx1 == gx2: # vertical wall
             x1 = gx1*grid_size+grid_size//4
             x2 = gx1*grid_size+3*grid_size//4
             y1 = gy1*grid_size+grid_size//4
@@ -86,9 +78,11 @@ def convert_walls():
         wall_rects.append((x1, y1, x2, y2))
 
 def draw_walls():
-    for i in range(len(wall_rects)):
+    for i in range(len(wall_rects)-1):
         gx1, gy1, gx2, gy2 = wall_rects[i]
-        pygame.draw.rect(screen, "blue", (gx1, gy1, gx2-gx1, gy2 - gy1))
+        pygame.draw.rect(screen, "blue", (gx1, gy1, gx2-gx1, gy2-gy1))
+    gx1, gy1, gx2, gy2 = wall_rects[len(wall_rects)-1]
+    pygame.draw.rect(screen, "pink", (gx1, gy1, gx2-gx1, gy2-gy1))
 
 def collision():
     for k in range(len(wall_rects)):
@@ -100,29 +94,47 @@ def collision():
 def ccr(cx, cy, r, x1, x2, y1, y2):
     x = max(x1, min(cx, x2))
     y = max(y1, min(cy, y2))
-    if ((cx - x) * (cx - x) + (cy - y) * (cy - y)) < r*r:
+    if ((cx-x)*(cx-x)+(cy-y)*(cy-y)) < r*r:
         return True
     else:
         return False
 
 running = True
+convert_walls()
+#pacman = pacman0
+pacman_home_x = 9
+pacman_home_y = 7
+px = pacman_home_x*grid_size+grid_size//2
+py = pacman_home_y*grid_size+grid_size//2
+pa = 0
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     keypressed = pygame.key.get_pressed()
-    if keypressed[pygame.K_LEFT] and not collision():
+    #pacman = pacman0
+    if keypressed[pygame.K_LEFT]:
         px -= 5
         pa = 180
-    if keypressed[pygame.K_RIGHT] and not collision():
+        if collision():
+            px += 5
+    if keypressed[pygame.K_RIGHT]:
         px += 5
         pa = 0
-    if keypressed[pygame.K_UP] and not collision():
+        if collision():
+            px -= 5
+    if keypressed[pygame.K_UP]:
         py -= 5
         pa = 90
-    if keypressed[pygame.K_DOWN] and not collision():
+        if collision():
+            py += 5
+    if keypressed[pygame.K_DOWN]:
         py += 5
         pa = -90
+        if collision():
+            py -= 5
+
     eat_pellets()
     draw_map()
     draw_player(pacman)
